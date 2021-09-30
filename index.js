18814e06e81026d51b9984fbc98b406a0c24896e
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-09-30 02:13:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main=document.querySelector("#main")
+main.remove()
+
+const h1=document.createElement("h1")
+h1.id="victory"
+const newHeader=h1
+
+newHeader.innerHTML="YOUR-NAME is the champion"
